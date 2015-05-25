85d7bfac4a779a17d6a61dc6476b077b0c5ed432
coresync <rungee@gmx.de>
2015-05-26 00:26:43 +0200

    Markt Button added

--- Altis_Life.Altis/dialog/player_inv.hpp
+++ Altis_Life.Altis/dialog/player_inv.hpp
@@ -237,6 +237,17 @@ class playerSettings {
 			h = (1 / 25);
 		};
 		
+		class ButtonMarket : Life_RscButtonMenu {
+			idc = -1;
+			text = "Market";
+			onButtonClick = "createDialog ""life_dynmarket_prices"";";
+			//x = 0.26 + (6.25 / 19.8) + (1 / 250 / (safezoneW / safezoneH));
+			x = 0.26 + (6.25 / 19.8) + (1 / 250 / (safezoneW / safezoneH));
+			y = 0.805;
+			w = (6.25 / 40);
+			h = (1 / 25);
+		};
+		
 		class Licenses_Menu : Life_RscControlsGroup
 		{
 			idc = -1;
